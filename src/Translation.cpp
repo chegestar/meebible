6b299b58df78de7b21977d5809e66e284b6efb4b
Ilya Skriblovsky <IlyaSkriblovsky@gmail.com>
2012-02-17 07:45:10 +0000

    Changes to headers style

---
+++ src/Translation.cpp
@@ -0,0 +1,78 @@
+#include "Translation.h"
+
+#include <QHash>
+#include <QByteArray>
+
+
+
+Translation::Translation()
+{
+    QHash<int, QByteArray> roleNames;
+    roleNames[Qt::DisplayRole] = "value";
+    roleNames[BookCodeRole] = "bookCode";
+    roleNames[BookNameRole] = "bookName";
+    setRoleNames(roleNames);
+}
+
+Translation::~Translation()
+{
+}
+
+
+
+int Translation::rowCount(const QModelIndex& index) const
+{
+    Q_UNUSED(index)
+
+    return bookCodes().size();
+}
+
+QVariant Translation::data(const QModelIndex& index, int role) const
+{
+    QStringList codes = bookCodes();
+
+    if (index.row() < 0  ||  index.row() >= codes.size())
+        return QVariant();
+
+    switch (role)
+    {
+        case BookCodeRole: return codes.at(index.row());
+
+        case Qt::DisplayRole:
+        case BookNameRole:
+            return bookName(codes.at(index.row()));
+    }
+
+    return QVariant();
+}
+
+
+
+int Translation::chaptersInBook(const QString& bookCode) const
+{
+    return verseCounts(bookCode).size();
+}
+
+int Translation::versesInChapter(const QString& bookCode, int chapterNo) const
+{
+    return verseCounts(bookCode).at(chapterNo - 1);
+}
+
+
+
+QString Translation::bookCodeAt(int row) const
+{
+    QStringList codes = bookCodes();
+
+    if (row >= 0 && row < codes.size())
+        return codes.at(row);
+
+    return "";
+}
+
+
+
+bool Translation::hasBook(const QString& bookCode) const
+{
+    return bookCodes().indexOf(bookCode) != -1;
+}
